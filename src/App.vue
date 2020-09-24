d0f7272003b10f50fa5ae906f0bca4d25a1e60c3
timibadass <omoyenitimi@gmail.com>
2020-09-24 19:14:58 +0100

    add notifications

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,61 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">Notifications</router-link>
     </div>
-    <router-view/>
+    <notifications
+      :group="group"
+      :type="type"
+      :duration="duration"
+      :position="position"
+    />
+    <router-view />
   </div>
 </template>
-
+<script>
+  export default {
+    data() {
+      return {
+        duration: -1,
+        group: "demo",
+        position: "top center",
+        type: "info",
+      };
+    },
+  };
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav {
-  padding: 30px;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+
+  .vue-notification.info {
+    border-left: 0;
+    background-color: orange;
+  }
+  .vue-notification.success {
+    border-left: 0;
+    background-color: limegreen;
+  }
+  .vue-notification.error {
+    border-left: 0;
+    background-color: red;
+  }
 </style>
---
+++ src/views/Notifications.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="notifications">
+    <h1>Notifications page</h1>
+    <button @click="show">Click me to show notification</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: "notifications-page",
+    methods: {
+      show() {
+        this.$notify({
+          group: "demo",
+          type: "error",
+          text: "This is an error notification",
+        });
+      },
+    },
+  };
+</script>
